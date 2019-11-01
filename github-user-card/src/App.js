3df23f727afdedc58922aea4b7f1592a26f9409c
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-10-31 23:31:16 -0400

    Added axios

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {}
+    this.state = {
+      userInfo: ""
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users/tiffany-simionescu')
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          userInfo: res.data
+        })
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
     return (
       <div>
-        
+        <h1>{this.state.userInfo.login}</h1>
       </div>
     )
   }
